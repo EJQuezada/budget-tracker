302b35bd28033da98f00eafb5db697b4e385b43b
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-04 11:54:30 +0200

    fix typos in layout.tsx file

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,11 @@
-import Logo from '@/components/logo';
-import React, { ReactNode } from 'react';
+import Logo from "@/components/logo";
+import React, { ReactNode } from "react";
 
 function layout({ children }: { children:ReactNode }) {
     return (
-        <div className='relative flex h-screen w-full flex-col items-center justify-center'>
+        <div className="relative flex h-screen w-full flex-col items-center justify-center">
             <Logo />
-            <div className='mt-12'>{children}</div>
+            <div className="mt-12">{children}</div>
         </div>
     );
 }
