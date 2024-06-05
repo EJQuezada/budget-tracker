a4bebdf386287f54a33b4a161a47976e0b6d7131
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-05 14:36:24 +0200

    update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import Logo from "@/components/Logo";
 import React, { ReactNode } from "react";
 
-function layout({ children }: { children:ReactNode }) {
+function layout({ children }: { children: ReactNode }) {
     return (
         <div className="relative flex h-screen w-full flex-col items-center justify-center">
             <Logo />
