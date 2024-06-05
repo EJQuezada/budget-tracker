e2bcfda8d302c0017408e49ec4bf4eca1691a0dd
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-05 11:30:50 +0200

    update logo.tsx file

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,22 @@
+"use client";
 import Logo from "@/components/Logo";
+import { link } from "fs";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 function Navbar() {
   return ( 
-  <>
-  
-  <DesktopNavbar />
-  </>
+    <>
+        <DesktopNavbar />
+    </>
   );
 }
+
+const items = [
+    {label: "Dashboard", link: "/" },
+    {label: "Transactions", link: "/transactions" },
+    {label: "Manage", link: "/manage" },
+];
 
 function DesktopNavbar() {
     return (
@@ -16,10 +24,31 @@ function DesktopNavbar() {
             <nav className="container flex items-center justify-between px-8">
                 <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
                     <Logo />
+                    <div className="flex h-full">
+                        {items.map(item => (
+                            <NavbarItem
+                            key={item.label}
+                            link={item.link}
+                            label={item.label}
+                            />
+                        ))}
+                    </div>
                 </div>
             </nav>
         </div>
     );
+}
+
+function NavbarItem({link, label}: {
+    link: string;
+    label: string
+}) {
+    const pathname = usePathname();
+    const isActive = pathname === link;
+
+    return (
+        <div className="relative flex items-center"></div>;
+    )
 }
 
 export default Navbar;
