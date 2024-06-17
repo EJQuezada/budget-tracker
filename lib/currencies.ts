15df98e2d63c0d55c56f6ea158d29bbef73f063b
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-17 17:40:30 +0200

    update typeof Currencies function

--- lib/currencies.ts
+++ lib/currencies.ts
@@ -3,4 +3,6 @@ export const Currencies = [
     {value: "EUR", label: "€ Euro", locale:"de-DE" },
     {value: "JPY", label: "¥ Yen", locale:"ja-JP" },
     {value: "GBP", label: "£ Pound", locale:"en-GB" },
-]
+];
+
+export type Currency = (typeof Currencies)[0];
