02cd84a1629a4820b79e11da84da5472131e489e
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-17 14:10:02 +0200

    replace layout with children in layout.tsx

--- app/wizard/layout.tsx
+++ app/wizard/layout.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode } from "react";
 
-function layout({children}: {children: ReactNode }) {
-    return <div>
-      layout
+function layout({ children }: {children: ReactNode }) {
+    return <div className="relative flex h-screen w-full flex-col items-center justify-center">
+      {children}
     </div>;
 }
 
