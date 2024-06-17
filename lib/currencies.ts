b1b5554128f6f26c3daf5ada1d7431e2d4b3f10d
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-17 17:17:04 +0200

    add currencies to currencies.tp file

---
+++ lib/currencies.ts
@@ -0,0 +1,6 @@
+export const Currencies = [
+    {value: "USD", label: "$ Dollar", locale:"en-US" },{value: "USD", label: "$ Dollar", locale:"en-US" },{value: "USD", label: "$ Dollar", locale:"en-US" },
+    {value: "EUR", label: "€ Euro", locale:"de-DE" },
+    {value: "JPY", label: "¥ Yen", locale:"ja-JP" },
+    {value: "GBP", label: "£ Pound", locale:"en-US" },
+]
