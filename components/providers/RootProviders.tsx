76faafd0e73c5a92457d42bd3afe910427ba0324
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-19 09:37:39 +0200

    update RootProviders.tsx and json packages with tanstack

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -2,17 +2,21 @@
 
 import { ThemeProvider } from "next-themes";
 import React, { ReactNode } from "react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 function RootProviders({ children }: { children: ReactNode }) {
+  const [queryClient] = React.useState(() => new QueryClient({}));
   return (
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-        >
-            {children}
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+          >
+              {children}
+          </ThemeProvider>
+        </QueryClientProvider>
   );
 }
 
