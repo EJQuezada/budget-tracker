02185c90bed826327733141aa8ed43421e457dd4
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-22 11:03:08 +0200

    update CreateTransactionDialog.tsx file

--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -3,6 +3,7 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { TransactionType } from "@/lib/types";
 import { cn } from "@/lib/utils";
+import { CreateTransactionSchema, CreateTransactionSchemaType } from "@/schema/transaction";
 import { ReactNode } from "react";
 
 interface Props {
@@ -11,8 +12,19 @@ interface Props {
 }
 
 import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 function CreateTransactionDialog({ trigger, type }: Props) {
+    const form = useForm<CreateTransactionSchemaType>({
+        resolver: zodResolver(CreateTransactionSchema),
+        defaultValues: {
+            type,
+            date: new Date(),
+        },
+    })
   return (
     <Dialog>
         <DialogTrigger asChild>{trigger}</DialogTrigger>
@@ -31,6 +43,25 @@ function CreateTransactionDialog({ trigger, type }: Props) {
                     transaction
                 </DialogTitle>
             </DialogHeader>
+            <Form {...form}>
+                <form className="space-y-4">
+                    <FormField 
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Description</FormLabel>
+                                <FormControl>
+                                    <Input defaultValue={""} {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    Transaction Description (optional)
+                                </FormDescription>
+                            </FormItem>
+                        )}
+                    />
+                </form>
+            </Form>
         </DialogContent>
     </Dialog>
   )
