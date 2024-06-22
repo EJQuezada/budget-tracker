82cbfd043859e13eea916b9570a2de7ba0ae4da4
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-22 10:26:47 +0200

    update CreateTransactionDialog.tsx file

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
+import CreateTransactionDialog from "./_components/CreateTransactionDialog";
 
 async function page() {
   const user = await currentUser();
@@ -29,18 +30,27 @@ async function page() {
         </p>
 
         <div className="flex items-center gap-3">
-          <Button 
-            variant={"outline"} 
-            className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
-          >
-            New income 🤑
-          </Button>
-          <Button 
+          <CreateTransactionDialog trigger={
+            <Button 
+              variant={"outline"} 
+              className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
+            >
+              New income 🤑
+            </Button>
+          }
+          type="income"
+          />
+
+          <CreateTransactionDialog trigger={
+            <Button 
             variant={"outline"} 
             className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white"
-          >
+            >
             New expense 😡
-          </Button>
+            </Button>
+          }
+          type="expense"
+          />
         </div>
       </div>
     </div>
