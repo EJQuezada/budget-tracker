9eb8b867117fa5df829d67abd4fd2a200e71ca00
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-22 10:46:16 +0200

    update syntax in transaction.ts file

--- schema/transaction.ts
+++ schema/transaction.ts
@@ -5,8 +5,9 @@ export const CreateTransactionSchema = z.object({
     description: z.string().optional(),
     date: z.coerce.date(),
     category: z.string(),
-    type: z.union({
-        z.literal("income"),
-        z.literal("expense")
-    }),
+    type: z.union([z.literal("income"), z.literal("expense")]),
 });
+
+export type CreateTransactionSchemaType = z.infer<
+    typeof CreateTransactionSchema
+>;
