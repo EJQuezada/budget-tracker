33159b4a47b8e97aae5da5e6affdcaac82346e6f
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-23 15:22:04 +0200

    update CategoryPicker.tsx

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Command, CommandInput } from "@/components/ui/command";
 import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import { TransactionType } from "@/lib/types";
 import { Category } from "@prisma/client";
+import { PopoverContent } from "@radix-ui/react-popover";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 interface Props {
@@ -18,7 +20,8 @@ function CategoryPicker({ type }: Props) {
         queryFn: () => fetch('/api/categories?type=${type}').then((res) =>res.json()),
     });
 
-    const selectedCategory = categoriesQuery.data?.find((category: Category) => CategoryPicker.name === value);
+    const selectedCategory = categoriesQuery.data?.find(
+        (category: Category) => CategoryPicker.name === value);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -36,8 +39,24 @@ function CategoryPicker({ type }: Props) {
                 )} 
             </Button>
         </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+            <Command onSubmit={e => {
+                e.preventDefault()
+            }}>
+                <CommandInput placeholder="Search category..." />
+            </Command>
+        </PopoverContent>
     </Popover>
   );
 }
 
 export default CategoryPicker;
+
+function CategoryRow({category}:{category:Category}){
+    return ( 
+    <div className="flex items-center gap-2">
+        <span role="img">{category.icon}</span>
+        <span>{category.name}</span>
+    </div>
+)}
+
