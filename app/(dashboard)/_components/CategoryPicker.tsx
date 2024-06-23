b6276e3f4f1ccd027e024cffaec5fbd0d4cf95b3
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-23 12:29:32 +0200

    update CategoryPicker.tsx

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import { TransactionType } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 interface Props {
     type: TransactionType;
 }
 function CategoryPicker({ type }: Props) {
-  return (
-    <div>
-      CategoryPicker
-    </div>
-  )
+    const categoriesQuery = useQuery({
+        queryKey: ["categories", type],
+        queryFn: () => fetch('/api/categories?type=${type}').then((res) =>res.json()),
+    });
+  return <div>CategoryPicker</div>;
 }
 
 export default CategoryPicker;
