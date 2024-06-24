bb2fd086ac57253e21758cbf23c21338d5ecd07e
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-24 21:35:08 +0200

    update categories.ts

---
+++ app/(dashboard)/_actions/categories.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { CreateCategorySchema, CreateCategorySchemaType } from "@/schema/categories";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export async function CreateCategory(form: CreateCategorySchemaType) {
+    const parsedBody = CreateCategorySchema.safeParse(form);
+    if (!parsedBody.success) {
+        throw new Error("bad request");
+    }
+
+    const user = await currentUser();
+
+    if (!user) {
+        redirect("/sign-in");
+    }
+}
