d88d5b81c660ed60f249bb1ec0447a07622bfbaa
Edgar Quezada <edgar.quezada@gmail.com>
2024-06-29 11:57:19 +0200

    update month and year history functions in transactions.ts

--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { TransactionType } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import { CreateTransactionSchema, CreateTransactionSchemaType } from "@/schema/transaction";
@@ -14,13 +14,13 @@ interface Props {
 import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import CategoryPicker from "./CategoryPicker";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import { format } from "date-fns";
-import { CalendarIcon } from "lucide-react";
+import { CalendarIcon, Loader2 } from "lucide-react";
 import { Calendar } from "@/components/ui/calendar";
 
 function CreateTransactionDialog({ trigger, type }: Props) {
@@ -133,18 +133,38 @@ function CreateTransactionDialog({ trigger, type }: Props) {
                                             </FormControl>
                                         </PopoverTrigger>
                                         <PopoverContent className="w-auto p-0">
-                                            <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
+                                            <Calendar 
+                                                mode="single" 
+                                                selected={field.value} 
+                                                onSelect={field.onChange} 
+                                                initialFocus 
+                                            />
                                         </PopoverContent>
                                     </Popover>
                                     <FormDescription>
                                         Select a date for this transaction
                                     </FormDescription>
+                                    <FormMessage />
                                 </FormItem>
                             )}
                         />
                     </div>
                 </form>
             </Form>
+            <DialogFooter>
+            <DialogClose asChild>
+                <Button type="button" variant={"secondary"} onClick={() => {
+                form.reset();
+                }}
+                >
+                Cancel
+                </Button> 
+            </DialogClose>
+            <Button onClick={form.handleSubmit(onSubmit)} disabled={isPending}>
+                {!isPending && "Create"}
+                {isPending && <Loader2 className="animate-spin" />}
+            </Button>
+        </DialogFooter> 
         </DialogContent>
     </Dialog>
   );
