0d3bb7ef6a32023e37df9a61626b8b2b486a33d7
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-02 23:14:44 +0200

    update Overview.tsx

--- app/(dashboard)/_components/Overview.tsx
+++ app/(dashboard)/_components/Overview.tsx
@@ -22,14 +22,14 @@ function Overview({ userSettings }: { userSettings: UserSettings }) {
                     initialDateFrom={dateRange.from}
                     initialDateTo={dateRange.to}
                     showCompare={false}
-                    onUpdate={values => {
+                    onUpdate={(values) => {
                         const {from, to} = values.range;
                         //We update the date range only if both dates are set
                         
-                        if (!from || !to)return;
-                        if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS){
-                            toast.error(`The selected date range is too big. Max allowed range is ${MAX_DATE_RANGE_DAYS} days!`
-
+                        if (!from || !to) return;
+                        if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+                            toast.error(
+                                `The selected date range is too big. Max allowed range is ${MAX_DATE_RANGE_DAYS} days!`
                             );
                             return;
                         }
