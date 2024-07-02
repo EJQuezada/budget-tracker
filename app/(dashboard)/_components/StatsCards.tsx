d666d48d9036e0634645b3b2192a88cd807c68fd
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-03 01:00:40 +0200

    updte StatsCards.tsx file

--- app/(dashboard)/_components/StatsCards.tsx
+++ app/(dashboard)/_components/StatsCards.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { GetBalanceStatsResponseType } from "@/app/api/stats/balance/route";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
 import { DateToUTCDate, GetFormatterForCurrency } from "@/lib/helpers";
 import { UserSettings } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
-import React, { useMemo } from "react";
+import { TrendingUp } from "lucide-react";
+import React, { ReactNode, useMemo } from "react";
 
 interface Props {
     from: Date;
@@ -26,8 +28,31 @@ function StatsCards({ from, to, userSettings }: Props) {
     }, [userSettings.currency]);
 
     const income = statsQuery.data?.income || 0;
+    const expense = statsQuery.data?.expense || 0;
 
-  return <div>StatsCards</div>;
+    const balance = income - expense;
+
+  return (
+    <div className="relative flex w-full flex-wrap gap-2 md:flex-nowrap">
+        <SkeletonWrapper isLoading={statsQuery.isFetching}>
+            <StatCard 
+                formatter={formatter}
+                value={income}
+                title="Income"
+                icon={
+                    <TrendingUp className="h-12 w-12 items-center rounded-lg p-2 text-emerald-500 bg-emerald-400/10" />
+                }
+            />
+        </SkeletonWrapper>
+    </div>
+  );
 }
 
 export default StatsCards;
+
+function StatCard({formatter, value, title, icon}: {
+    formatter: Intl.NumberFormat;
+    icon: ReactNode;
+    title: String;
+    value: number;
+}) {}
