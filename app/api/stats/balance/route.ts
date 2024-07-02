5175edc65dea817d5f8f91fe6dc0e5c8cddc4183
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-02 23:25:01 +0200

    create route.ts file with basic functions

---
+++ app/api/stats/balance/route.ts
@@ -0,0 +1,14 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export async function GET(request:Request) {
+    const user = await currentUser();
+    if (!user) {
+        redirect("/sign-in");
+    }
+
+    const {searchParams} = new URL(request.url);
+    const from = searchParams.get("from");
+    const to = searchParams.get("to");
+    
+}
