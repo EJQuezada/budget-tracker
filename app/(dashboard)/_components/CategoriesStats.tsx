254549af988fe4c3f193d0370dae5028e9c89635
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-04 22:39:44 +0200

    add CategoriesStats function to file with same name

--- app/(dashboard)/_components/CategoriesStats.tsx
+++ app/(dashboard)/_components/CategoriesStats.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import { UserSettings } from "@prisma/client";
 import React from "react";
 
-function CategoriesStats() {
+
+interface Props {
+    userSettings: UserSettings;
+    from: Date; 
+    to: Date;
+}
+function CategoriesStats({userSettings, from, to}: Props) {
   return <div>CategoriesStats</div>;
 }
 
