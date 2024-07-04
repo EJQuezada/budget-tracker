61dc20156456f227a1c2a48ace151593f17ba1e2
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-04 15:26:58 +0200

    update spacing in syntax of page.tsx file

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -31,15 +31,16 @@ async function page() {
         </p>
 
         <div className="flex items-center gap-3">
-          <CreateTransactionDialog trigger={
-            <Button 
-              variant={"outline"} 
-              className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
-            >
-              New income 🤑
-            </Button>
-          }
-          type="income"
+          <CreateTransactionDialog 
+            trigger={
+              <Button 
+                variant={"outline"} 
+                className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
+              >
+                New income 🤑
+              </Button>
+            }
+            type="income"
           />
 
           <CreateTransactionDialog trigger={
