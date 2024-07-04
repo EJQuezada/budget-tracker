cb73b44dd38dcdf9aba29062df810cd202eb6751
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-04 23:43:43 +0200

    update CategoriesStats.tsx file with CategoriesCard function

--- app/(dashboard)/_components/CategoriesStats.tsx
+++ app/(dashboard)/_components/CategoriesStats.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { GetCategoriesStatsResponseType } from "@/app/api/stats/categories/route";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
+import { DateToUTCDate, GetFormatterForCurrency } from "@/lib/helpers";
+import { TransactionType } from "@/lib/types";
 import { UserSettings } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useMemo } from "react";
 
 
 interface Props {
@@ -11,10 +15,35 @@ interface Props {
     to: Date;
 }
 function CategoriesStats({userSettings, from, to}: Props) {
-    const statsQuery = useQuery({
+    const statsQuery = useQuery<GetCategoriesStatsResponseType>({
         queryKey: ["overview", "stats", "categories", from, to],
+        queryFn: () => fetch(`/api/stats/categories?from=${DateToUTCDate (from)}&to=${DateToUTCDate(to)}`).then(res => res.json()),
     });
-  return <div>CategoriesStats</div>;
+
+    const formatter = useMemo(() => {
+        return GetFormatterForCurrency(userSettings.currency)
+    }, [userSettings.currency]);
+
+    return ( 
+        <div className="flex w-full flex-wrap gap-2 md:flex-nowrap">
+            <SkeletonWrapper isLoading={statsQuery.isFetching}>
+                <CategoriesCard
+                    formatter={formatter}
+                    type="income"
+                    data={statsQuery.data || []}
+                />
+            </SkeletonWrapper>
+        </div>
+    );
 }
 
 export default CategoriesStats;
+
+function CategoriesCard({data, type, formatter}: {
+    type: TransactionType;
+    formatter: Intl.NumberFormat;
+    data: GetCategoriesStatsResponseType;
+}) {
+    const filteredData = data.filter(el => el.type === type);
+    const total = filteredData.reduce((acc, el) => acc + (el._sum?.amount || 0));
+}
