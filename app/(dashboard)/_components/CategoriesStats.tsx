5360bd0bd118f734ea8c9205fd2ccbdde7e59aef
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-04 23:58:01 +0200

    Update CategoriesStats.tsx file with Card functions

--- app/(dashboard)/_components/CategoriesStats.tsx
+++ app/(dashboard)/_components/CategoriesStats.tsx
@@ -2,6 +2,7 @@
 
 import { GetCategoriesStatsResponseType } from "@/app/api/stats/categories/route";
 import SkeletonWrapper from "@/components/SkeletonWrapper";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { DateToUTCDate, GetFormatterForCurrency } from "@/lib/helpers";
 import { TransactionType } from "@/lib/types";
 import { UserSettings } from "@prisma/client";
@@ -45,5 +46,24 @@ function CategoriesCard({data, type, formatter}: {
     data: GetCategoriesStatsResponseType;
 }) {
     const filteredData = data.filter(el => el.type === type);
-    const total = filteredData.reduce((acc, el) => acc + (el._sum?.amount || 0));
+    const total = filteredData.reduce((acc, el) => acc + (el._sum?.amount || 0), 0);
+
+    return (
+        <Card className="h-80 w-full col-span-6">
+            <CardHeader>
+                <CardTitle className="grid grid-flow-row justify-between gap-2 text-muted-foreground md:grid-flow-col"></CardTitle>
+            </CardHeader>
+
+            <div className="flex items-center justify-between gap-2">
+                {filteredData.length === 0 && (
+                    <div className="flex h-60 w-full flex-col items-center justify-center">
+                        No data for the selected period
+                        <p className="text-sm text-muted-foreground">
+                            Try selecting a different period or try adding new {type ==="income" ? "incomes" : "expenses"}
+                        </p>
+                    </div>
+                )}
+            </div>
+        </Card>
+    )
 }
