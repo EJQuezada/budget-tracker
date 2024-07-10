2f36a6c782c316af5efd98ff1b477bfd14939487
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-11 00:51:53 +0200

    create and update route.ts in app/history-data file

--- app/api/history-data/route.ts
+++ app/api/history-data/route.ts
@@ -1,8 +1,17 @@
+import prisma from "@/lib/prisma";
+import { Period, Timeframe } from "@/lib/types";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const getHistoryDataSchema = z.object({
+    timeframe: z.enum(["month", "year"]),
+    month: z.coerce.number().min(0).max(11).default(0),
+    year: z.coerce.number().min(2000).max(3000),
+});
 
 export async function GET(request: Request) {
-    const user = currentUser();
+    const user = await currentUser();
     if (!user) {
         redirect("/sign-in");
     }
@@ -11,4 +20,89 @@ export async function GET(request: Request) {
     const timeframe = searchParams.get("timeframe");
     const year = searchParams.get("year");
     const month = searchParams.get("month");
+
+    const queryParams = getHistoryDataSchema.safeParse({
+        timeframe,
+        month,
+        year,
+    });
+
+    if (!queryParams.success) {
+        return Response.json(queryParams.error.message, {
+            status: 400,
+        });
+    }
+
+    const data = await getHistoryData(user.id, queryParams.data.timeframe,{
+        month: queryParams.data.month,
+        year: queryParams.data.year,
+    });
+}
+
+export type GetHistoryDataResponseType = Awaited<
+    ReturnType<typeof getHistoryData>
+>;
+
+async function getHistoryData(
+    userId: string, 
+    timeframe: Timeframe, 
+    period: Period
+) {
+    switch (timeframe) {
+        case "year":
+            return await getYearHistoryData(userId, period.year);
+            case "month":
+                return await getMonthHistoryData(userId, period.year, period.month);
+    }
+}
+
+type HistoryData = {
+    expense: number;
+    income: number;
+    year: number;
+    month: number;
+    day?: number; 
+};
+
+async function getYearHistoryData(userId: string, year: number) {
+    const result = await prisma.yearHistory.groupBy({
+        by: ["month"],
+        where: {
+           userId,
+           year
+        },
+        _sum: {
+            expense: true, 
+            income: true,
+        },
+        orderBy: [
+            {
+                month: "asc",
+            },
+        ],
+    });
+
+    if (!result || result.length=== 0) return [];
+
+    const history:HistoryData[] = [];
+
+    for (let i = 0; i < 12; i++) {
+        let expense = 0;
+        let income = 0;
+
+        const month = result.find((row) => row.month === i);
+        if (month) {
+            expense = month._sum.expense || 0;
+            income = month._sum.income || 0;
+        }
+
+        history.push({
+            year,
+            month: i,
+            expense,
+            income,
+        });
+    }
+
+    return history;
 }
