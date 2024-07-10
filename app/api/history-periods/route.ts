21d72b1e72fe67ea359f464ac98ff5c3cd18c96c
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-10 10:42:25 +0200

    update route.ts file  to call GetHistoryPeriods

--- app/api/history-periods/route.ts
+++ app/api/history-periods/route.ts
@@ -1,3 +1,4 @@
+import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -11,8 +12,30 @@ export async function GET(request: Request) {
     return Response.json(periods);
 }
 
-export type GetHistoryPeriodsResponseType = Awaited<ReturnType<typeof 
+export type GetHistoryPeriodsResponseType = Awaited<ReturnType<typeof getHistoryPeriods>
+>;
 
 async function getHistoryPeriods(userId: string) {
+    const result = await prisma.monthHistory.findMany({
+        where: {
+            userId,
+        },
+        select: {
+            year: true,
+        },
+        distinct: ["year"],
+        orderBy: [
+            {
+                year: "asc",
+            },
+        ],
+    });
 
+    const years = result.map(el => el.year);
+    if (years.length === 0) {
+        //Return the current year
+        return [new Date().getFullYear()];
+    }
+
+    return years;
 }
