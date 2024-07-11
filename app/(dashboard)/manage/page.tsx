d4483287b1a9e60ff77530c2568459b867ea457c
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-12 01:27:03 +0200

    update page.tsx  CategoryCard functions

--- app/(dashboard)/manage/page.tsx
+++ app/(dashboard)/manage/page.tsx
@@ -15,6 +15,9 @@ import { PlusSquare, TrendingDown, TrendingUp } from "lucide-react";
 import React from "react";
 import CreateCategoryDialog from "../_components/CreateCategoryDialog";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+import { Category } from "@prisma/client";
 
 function page() {
   return (
@@ -58,6 +61,8 @@ function CategoryList({type}:{ type: TransactionType }) {
         queryFn: () => fetch(`/api/categories?type=${type}`).then((res) => res.json()),
     });
 
+    const dataAvailable = categoriesQuery.data && categoriesQuery.data.length > 0;
+
     return (
         <SkeletonWrapper isLoading={categoriesQuery.isFetching}>
             <Card>
@@ -89,7 +94,45 @@ function CategoryList({type}:{ type: TransactionType }) {
                         />
                     </CardTitle>
                 </CardHeader>
+                <Separator />
+                {
+                    !dataAvailable && (
+                        <div className="flex h-40 w-full items-center justify-center">
+                            <p>
+                                No
+                                <span 
+                                    className={cn(
+                                        "m-1", 
+                                        type === "income" ? "text-emerald-500" : "text-red-500"
+                                    )}
+                                >
+                                    {type}
+                                </span> 
+                                categories yet 
+                            </p>
+
+                            <p className="text-sm text-muted-foreground">
+                                Create a category to get started
+                            </p>
+                        </div>
+                    )
+                }
+                {
+                    dataAvailable && (
+                        <div className="grid grid-flow-row gap-2 p-2 sm:grid-flow-row sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                            {categoriesQuery.data.map((category: Category) => (
+                                <CategoryCard category={category} key={category.name} />
+                            ))}
+                        </div>
+                    )
+                }
             </Card>
         </SkeletonWrapper>
-    )
+    );
+}
+
+function CategoryCard({ category }:{ category:Category }) {
+    return (
+        <div className="flex border-separate flex-col justify-between rounded-md border shadow-md shadow-black/[0.1] dark:shadow-white/[0.1]"></div>
+    );
 }
