562c282be5afca3bb0afc64d6c6cfdd78c26044b
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-12 00:52:49 +0200

    update CreateCategoryDialog.tsx file with DialogTrigger funcs

--- app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -98,10 +98,17 @@ function CreateCategoryDialog({ type, successCallback, trigger }: Props) {
 
   return <Dialog open={open} onOpenChange={setOpen}>
     <DialogTrigger asChild>
-        <Button variant={"ghost"} className="flex border-separate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground">
-            <PlusSquare className="mr-2 h-4 w-4" />
-            Create new 
-        </Button>
+        {trigger ? (
+            trigger
+         ) : (
+            <Button 
+                variant={"ghost"} 
+                className="flex border-separate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground"
+            >
+                <PlusSquare className="mr-2 h-4 w-4" />
+                Create new 
+            </Button>
+        )}
     </DialogTrigger>
     <DialogContent>
         <DialogHeader>
