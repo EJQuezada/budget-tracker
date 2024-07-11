0362d1300dcb7e33dc890ad6933dc1bdd6ef972f
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-11 22:28:28 +0200

    upate History.tsx file with CountUp functions

--- app/(dashboard)/_components/History.tsx
+++ app/(dashboard)/_components/History.tsx
@@ -215,6 +215,19 @@ function TooltipRow({
     return (
         <div className="flex items-center gap-2">
             <div className={cn("h-4 w-4 rounded-full", bgColor)} />
+            <div className="flex w-full justify-between">
+                <p className="text-sm text-muted-foreground">{label}</p>
+                <div className={cn("text-sm font-bold", textColor)}>
+                    <CountUp
+                        duration={0.5}
+                        preserveValue
+                        end={value}
+                        decimals={0}
+                        formattingFn={(value) => formatter.format(value)}
+                        className="text-sm"
+                    />
+                </div>
+            </div>
         </div>
     );
 }
