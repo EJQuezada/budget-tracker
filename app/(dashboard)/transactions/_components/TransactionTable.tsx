9977453b98869a112362433d769a98835d6c305e
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-14 23:43:27 +0200

    update package-lock.json, package.json and TransactionTable.tsx files

--- app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -3,12 +3,46 @@
 import { DateToUTCDate } from "@/lib/helpers";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { 
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+import { GetTransactionHistoryResponseType } from "@/app/api/transactions-history/route";
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+
 interface Props {
     from: Date;
     to: Date;
 }
+
+type TransactionHistoryRow = GetTransactionHistoryResponseType[0];
+
+export const columns: ColumnDef<TransactionHistoryRow>[] = [
+    {
+        accessorKey: "category",
+        cell: ({row}) => (
+            <div className="flex gap-2 capitalize">
+                {row.original.categoryIcon}
+                <div className="capitalize">
+                    {row.original.category}
+                </div>
+            </div>
+        ),
+    },
+];
+
 function TransactionTable({ from, to }: Props) {
-    const history = useQuery({
+    const history = useQuery<GetTransactionHistoryResponseType>({
         queryKey: ["transactions", "history", from, to],
         queryFn: () => 
             fetch(
@@ -17,7 +51,7 @@ function TransactionTable({ from, to }: Props) {
                 )}&to=${DateToUTCDate(to)}`
             ).then(res => res.json()),
     });
-    
+
   return <div>TransactionTable</div>;
 }
 
