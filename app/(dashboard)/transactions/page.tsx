ecc83fb368cd962764919c248aae6ecc0661a397
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-14 09:47:52 +0200

    add Transaction History text to transactions folder

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -3,7 +3,15 @@
 import React from 'react'
 
 function page() {
-  return <div>page</div>;
+  return <div className="border-b bg-card">
+    <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+        <div>
+            <p className="text-3xl font-bold">
+                Transactions History
+            </p>
+        </div>
+    </div>
+  </div>;
 }
 
 export default page;
