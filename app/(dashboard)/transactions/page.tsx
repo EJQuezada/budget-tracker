5d05cea38a4c42500110ab96fb3109d6e21c6b21
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-14 12:21:38 +0200

    add DateRangePicker function to .\transactions\page.tsx file

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,17 +1,46 @@
 "use client";
 
-import React from 'react'
+import { DateRangePicker } from '@/components/ui/date-range-picker';
+import { MAX_DATE_RANGE_DAYS } from '@/lib/constants';
+import { differenceInDays, startOfMonth } from 'date-fns';
+import React, { useState } from 'react'
+import { toast } from 'sonner';
 
-function page() {
-  return <div className="border-b bg-card">
-    <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
-        <div>
-            <p className="text-3xl font-bold">
-                Transactions History
-            </p>
+function TransactionsPage() {
+    const [dateRange, setDateRange] = useState<{from: Date; to: Date}>({
+        from: startOfMonth(new Date()),
+        to: new Date(),
+    });
+  return ( 
+    <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+            <div>
+                <p className="text-3xl font-bold">
+                    Transactions History
+                </p>
+            </div>
         </div>
+        <DateRangePicker 
+            initialDateFrom={dateRange.from}
+            initialDateTo={dateRange.to}
+            showCompare={false}
+            onUpdate={(values) => {
+                const {from, to} = values.range;
+                //We update the date range only if both dates are set
+                
+                if (!from || !to) return;
+                if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+                    toast.error(
+                        `The selected date range is too big. Max allowed range is ${MAX_DATE_RANGE_DAYS} days!`
+                    );
+                    return;
+                }
+
+                setDateRange({ from, to });
+            }}
+        />
     </div>
-  </div>;
+  );
 }
 
-export default page;
+export default TransactionsPage;
