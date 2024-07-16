ee0ee6057864523afae840c4e74be275ca32af89
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-16 12:11:40 +0200

    update DeleteTransactionDialog.tsx file

--- app/(dashboard)/transactions/_components/DeleteTransactionDialog.tsx
+++ app/(dashboard)/transactions/_components/DeleteTransactionDialog.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
 import React from "react";
+import { toast } from "sonner";
 
 interface Props {
     open: boolean;
@@ -10,7 +12,50 @@ interface Props {
 
 function DeleteTransactionDialog({ open, setOpen, transactionId }: Props) 
 {
-    return <div>DeleteTransactionDialog</div>;
+    const queryClient = useQueryClient();
+
+    const deleteMutation = useMutation({
+      mutationFn: DeleteTransaction,
+      onSuccess: async () => {
+        toast.success("Transaction deleted successfully", {
+          id: transactionId,
+        });
+
+        await queryClient.invalidateQueries({
+          queryKey: ["transactions"],
+        });
+      },
+      onError: () => {
+        toast.error("Something went wrong", {
+          id: transactionId,
+        });
+      },
+    });
+    return (
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your category
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction 
+              onClick={() => {
+                toast.loading("Deleting transaction...", {
+                  id: transactionId,
+                });
+                deleteMutation.mutate(transactionId);
+              }}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    );
 }
 
 export default DeleteTransactionDialog;
