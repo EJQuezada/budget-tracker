44ff0fd649b2ff64e0b17f7e9d2b5059ee821f18
Edgar Quezada <edgar.quezada@gmail.com>
2024-07-16 11:48:11 +0200

    create DeleteTransactionDialog.tsx file with basic functions

---
+++ app/(dashboard)/transactions/_components/DeleteTransactionDialog.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react';
+
+function DeleteTransactionDialog() {
+    return <div>DeleteTransactionDialog</div>;
+}
+
+export default DeleteTransactionDialog;
--- app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -299,22 +299,25 @@ function RowActions({transaction}:{transaction:TransactionHistoryRow}){
     const [showDeleteDialog, setShowDeleteDialog] = useState(false);
 
     return(
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant={"ghost"} className="h-8 w-8 p-0">
-                    <span className="sr-only">Open menu</span>
-                    <MoreHorizontal className="h-4 w-4" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem className="flex items-center gap-2" onSelect={() => {setShowDeleteDialog(prev => !prev)}}>
-                    <TrashIcon className="h-4 w-4 text-muted-foreground" />
-                    Delete
-                </DropdownMenuItem>
-            </DropdownMenuContent>
+        <>
+        <DeleteTransactionDialog open={showDeleteDialog} setOpen={setShowDeleteDialog} transactionId={transaction.id} />
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant={"ghost"} className="h-8 w-8 p-0">
+                        <span className="sr-only">Open menu</span>
+                        <MoreHorizontal className="h-4 w-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="flex items-center gap-2" onSelect={() => {setShowDeleteDialog(prev => !prev)}}>
+                        <TrashIcon className="h-4 w-4 text-muted-foreground" />
+                        Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
 
-        </DropdownMenu>
+            </DropdownMenu>
+        </>
     )
 }
